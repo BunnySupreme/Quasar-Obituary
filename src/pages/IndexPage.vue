5cf18b472c3022e4ca8fc6473b930417c140b0df
Felix Schober <felixschober1998@gmail.com>
2025-07-20 19:14:49 +0200

    stepper for form

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,43 @@
 <template>
   <q-page class="q-pa-md">
-    <h1>Obituary Wizard</h1>
+    <q-stepper
+      v-model="step"
+      vertical
+      animated
+    >
+      <q-step
+        :name="1"
+        title="Personal Info"
+        subtitle="Name and dates"
+      >
+        <q-stepper-navigation>
+          <q-btn @click="step = 2" label="Next" color="primary"></q-btn>
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="2"
+        title="Biography"
+        subtitle="Life story"
+      >
+        <q-stepper-navigation>
+          <q-btn flat @click="step = 1" label="Back"/>
+          <q-btn @click="step = 3" label="Next" color="primary"/>
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step :name="3" title="Template" subtitle="Choose a layout">
+        <q-stepper-navigation>
+          <q-btn flat @click="step = 2" label="Back" />
+          <q-btn color="positive" label="Finish" />
+        </q-stepper-navigation>
+      </q-step>
+    </q-stepper>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const step = ref(1)
 </script>
